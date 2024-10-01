2a9b6f05790647c02deedc7809c7e21afc013890
ken <ccnhs_cubilo0098@calbayog.ph.education>
2024-10-01 23:26:56 +0800

    WIP

---
+++ constant/index.js
@@ -0,0 +1,264 @@
+export const IcedCoffee = [
+  {
+    id: "icedcoffee1",
+    imgUrl: "/image/IcedCoffee/icedamericano.png",
+    title: "Americano",
+    price: "₱80",
+    quantity: "1",
+    Stocks: "30",
+  },
+  {
+    id: "icedcoffee2",
+    imgUrl: "/image/IcedCoffee/icedcaffelatte.png",
+    title: "Caffe Latte",
+    price: "₱90",
+    quantity: "1",
+    Stocks: "40",
+  },
+  {
+    id: "icedcoffee3",
+    imgUrl: "/image/IcedCoffee/icedkopi.png",
+    title: "Kopi",
+    price: "₱90",
+    quantity: "1",
+    Stocks: "50",
+  },
+  {
+    id: "icedcoffee4",
+    imgUrl: "/image/IcedCoffee/icedmochadownunder.png",
+    title: "Mocha Down Under",
+    price: "₱120",
+    quantity: "1",
+    Stocks: "20",
+  },
+  {
+    id: "icedcoffee5",
+    imgUrl: "/image/IcedCoffee/icedspanishlatte.png",
+    title: "Spanish Latte",
+    price: "₱90",
+    quantity: "1",
+    Stocks: "40",
+  },
+
+  {
+    id: "icedcoffee6",
+    imgUrl: "/image/IcedCoffee/icedflavoredlatte.png",
+    title: "Flavored Latte",
+    price: "₱120",
+    quantity: "1",
+    Stocks: "40",
+  },
+  {
+    id: "icedcoffee7",
+    imgUrl: "/image/IcedCoffee/icedwhitechocolatemocha2.png",
+    title: "White Chocolate Mocha",
+    price: "₱120",
+    quantity: "1",
+    Stocks: "25",
+  },
+
+  //-----Add-ons/Extra-----
+  // {
+  //     id:'coffee1',
+  //     imgUrl:'/image/americano.webp',
+  //     title:'Expresso Hot',
+  //     price:'₱20'
+  // },
+  // {
+  //     id:'coffee2',
+  //     imgUrl:'/image/CaffeLatte.jpeg',
+  //     title:'Whipped Cream',
+  //     price:'₱20'
+  // },
+  // {
+  //     id:'coffee3',
+  //     imgUrl:'/image/Kopi.jpg',
+  //     title:'Extra Flavor Pump',
+  //     price:'₱20'
+  // },
+];
+
+export const HotCoffee = [
+  // Hot Coffee section
+  {
+    id: "hotcoffee1",
+    imgUrl: "/image/HotCoffee/hotamericano.png",
+    title: "Hot Americano",
+    price: "₱80",
+    quantity: "1",
+    Stocks: "40",
+  },
+  {
+    id: "hotcoffee2",
+    imgUrl: "/image/HotCoffee/hotlatte2.png",
+    title: "Hot Latte | Capp",
+    price: "₱90",
+    quantity: "1",
+    Stocks: "50",
+  },
+  {
+    id: "hotcoffee3",
+    imgUrl: "/image/HotCoffee/hotkopi2.png",
+    title: "Hot Kopi",
+    price: "₱90",
+    quantity: "1",
+    Stocks: "50",
+  },
+  {
+    id: "hotcoffee5",
+    imgUrl: "/image/HotCoffee/hotmochadownunder2.png",
+    title: "Hot Mocha Down Under",
+    price: "₱120",
+    quantity: "1",
+    Stocks: "35",
+  },
+  {
+    id: "hotcoffee4",
+    imgUrl: "/image/HotCoffee/hotspanishlatte.png",
+    title: "Hot Spanish Latte",
+    price: "₱90",
+    quantity: "1",
+    Stocks: "30",
+  },
+
+  {
+    id: "hotcoffee6",
+    imgUrl: "/image/HotCoffee/hotflavoredlatte.png",
+    title: "Hot Flavored Latte",
+    price: "₱120",
+    quantity: "1",
+    Stocks: "40",
+  },
+  {
+    id: "hotcoffee7",
+    imgUrl: "/image/HotCoffee/hotwhitechocolatemocha2.png",
+    title: "Hot White Chocolate Mocha",
+    price: "₱120",
+    quantity: "1",
+    Stocks: "37",
+  },
+];
+
+export const CreationSeries = [
+  {
+    id: "CreationSeries1",
+    imgUrl: "/image/CreationSeries/baristachoice.png",
+    title: "Barista Choice",
+    price: "₱120",
+    quantity: "1",
+    Stocks: "29",
+  },
+  {
+    id: "CreationSeries3",
+    imgUrl: "/image/CreationSeries/toffeenutcreme.png",
+    title: "Tofee Nut Creme",
+    price: "₱140",
+    quantity: "1",
+    Stocks: "34",
+  },
+  {
+    id: "CreationSeries2",
+    imgUrl: "/image/CreationSeries/whitemochabreve.png",
+    title: "White Mocha Breve",
+    price: "₱120",
+    quantity: "1",
+    Stocks: "33",
+  },
+];
+
+export const NonCoffee = [
+  {
+    id: "NonCoffee1",
+    imgUrl: "/image/NonCoffee/summerfling.png",
+    title: "Summer Fling",
+    price: "₱70",
+    quantity: "1",
+    Stocks: "23",
+  },
+  {
+    id: "NonCoffee2",
+    imgUrl: "/image/NonCoffee/sodacooler2.png",
+    title: "Soda Cooler",
+    price: "₱70",
+    quantity: "1",
+    Stocks: "36",
+  },
+  {
+    id: "NonCoffee3",
+    imgUrl: "/image/NonCoffee/xocolatte.png",
+    title: "Xocolatte",
+    price: "₱120",
+    quantity: "1",
+    Stocks: "29",
+  },
+  {
+    id: "NonCoffee4",
+    imgUrl: "/image/NonCoffee/greentealatte.png",
+    title: "Green Tea Latte",
+    price: "₱120",
+    quantity: "1",
+    Stocks: "18",
+  },
+];
+
+export const Frappes = [
+  {
+    id: "Frappes1",
+    imgUrl: "/image/Frappes/javachipsfrappuccino.png",
+    title: "Java Chips Cappuccino",
+    price: "₱130",
+    quantity: "1",
+    Stocks: "23",
+  },
+  {
+    id: "Frappes3",
+    imgUrl: "/image/Frappes/greenteamatcha.png",
+    title: "Green Tea Matcha",
+    price: "₱130",
+    quantity: "46",
+    Stocks: "100",
+  },
+  {
+    id: "Frappes2",
+    imgUrl: "/image/Frappes/cookiesandcream.png",
+    title: "Cookies and Cream",
+    price: "₱130",
+    quantity: "1",
+    Stocks: "45",
+  },
+  {
+    id: "Frappes4",
+    imgUrl: "/image/Frappes/whitechocolatemocha.png",
+    title: "White Chocolate Mocha",
+    price: "₱130",
+    quantity: "1",
+    Stocks: "33",
+  },
+  {
+    id: "Frappes5",
+    imgUrl: "/image/Frappes/strawberrycreme.png",
+    title: "Strawberry Creme",
+    price: "₱130",
+    quantity: "1",
+    Stocks: "46",
+  },
+];
+
+export const Pastries = [
+  {
+    id: "Pastries1",
+    imgUrl: "/image/Pastries/ensaymada2.png",
+    title: "Classic Ensaymada",
+    price: "₱45",
+    quantity: "1",
+    Stocks: "15",
+  },
+  {
+    id: "Pastries2",
+    imgUrl: "/image/Pastries/cookies.png",
+    title: "Cookies",
+    price: "₱39",
+    quantity: "1",
+    Stocks: "20",
+  },
+];
--- sections/menu.jsx
+++ sections/menu.jsx
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from "react";
 import Section from "@/components/Section";
 import CoffeeCard from "@/components/CoffeeCard";
+
 import { RunningText, TitlePage } from "@/components/TypingText";
 
-const OurMenu = ({ setCardLength }) => {
+import { HotCoffee } from "@/constant"; //HotCoffee
+import HotcoffeeCard from "@/components/HotCoffeeCard";
+
+//setCardLength sa props
+const OurMenu = ({ setTotalQuantity, setCardLength }) => {
   const [products, setProducts] = useState([]);
   const [activeProduct, setActiveProduct] = useState(null);
+  
+  const [hotcoffeeActive, sethotcoffeeActive] = useState("hotcoffee5");
 
   useEffect(() => {
+     
+
+
     // Fetch coffee products from the Fake Coffee API
     fetch("https://fake-coffee-api.vercel.app/api")
       .then((res) => res.json())
@@ -18,28 +28,44 @@ const OurMenu = ({ setCardLength }) => {
   }, []);
 
   return (
-    <Section id="coffee-menu">
-      <RunningText />
-      <TitlePage title="Coffee Menu" />
+    <>
+      <Section id="coffee-menu">
+        <RunningText />
+        <TitlePage title="Coffee Menu" />
 
-      <div id="coffee-menu" style={{ overflowX: "auto" }}>
-        <div className="mt-[40px] flex flex-row min-h-[70vh] gap-4">
-          {products.length > 0 ? (
-            products.map((product) => (
-              <CoffeeCard
-                key={product.id}
-                {...product}
-                active={activeProduct === product.id}
-                handleClick={() => setActiveProduct(product.id)}
-                setCardLength={setCardLength}
-              />
-            ))
-          ) : (
-            <p>Loading products...</p>
-          )}
+        <div id="coffee-menu" style={{ overflowX: "auto" }}>
+          <div className="mt-[40px] flex flex-row min-h-[70vh] gap-4">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CoffeeCard
+                  key={product.id}
+                  {...product}
+                  active={activeProduct === product.id}
+                  handleClick={() => setActiveProduct(product.id)}
+                  setCardLength={setCardLength}
+                />
+              ))
+            ) : (
+              <p>Loading products...</p>
+            )}
+          </div>
         </div>
-      </div>
-    </Section>
+      </Section>
+
+      <Section id="hotcoffee">
+        <RunningText/>
+        <TitlePage title="Hot Coffee" />
+
+        <div id="hotcoffee" style={{ overflowX: 'auto' }}>
+          <div className="mt-[50px] flex flex-row min-h-[70vh] gap-2">
+            {HotCoffee.map((coffee, index) => (
+              <HotcoffeeCard key={coffee.id} {...coffee} active={hotcoffeeActive} handleClick={sethotcoffeeActive} setTotalQuantity={setTotalQuantity}/>
+            ))}
+          </div>
+        </div>
+      </Section>
+
+    </>
   );
 };
 
